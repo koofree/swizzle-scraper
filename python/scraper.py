89c875b4fc9ef552717bff07045a55a8bb0a0296
kool22 <yongku84@gmail.com>
2017-05-18 19:17:20 +0900

    make the function for main work.

--- python/scraper.py
+++ python/scraper.py
@@ -14,6 +14,32 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(ROOT_DIR, '../config.json'), 'r') as config_file:
     config_json = json.load(config_file)
 
+
+def scrape(_scraper_name, _target_id):
+    package_name = config_json['scrapers'][_scraper_name]['package']
+    connector_name = config_json['scrapers'][_scraper_name]['connector']
+    writer_name = config_json['scrapers'][_scraper_name]['writer']
+    limit_count = config_json['scrapers'][_scraper_name]['limit']
+    model_name = None
+    if 'model' in config_json['scrapers'][_scraper_name]:
+        model_name = config_json['scrapers'][_scraper_name]['model']
+
+    conn = importlib.import_module('connectors.' + connector_name).Connector(config_json['connectors'])
+    writer = importlib.import_module('writers.' + writer_name).Writer(config_json['writers'], name=_target_id)
+    scraper = importlib.import_module('scrapers.' + package_name).Scraper(conn.api())
+    model = None
+    if model_name:
+        model = importlib.import_module('models.' + model_name).Model
+
+    for item in scraper.scrape(_target_id, limit=limit_count):
+        if model:
+            from_func = getattr(model, "from_" + connector_name)
+            t = from_func(item)
+            writer.write(t)
+        else:
+            writer.write(item)
+
+
 if __name__ == '__main__':
     scraper_name = config_json['default']
 
@@ -28,25 +54,4 @@ if __name__ == '__main__':
         scraper_name = sys.argv[1]
         target_id = sys.argv[2]
 
-    package_name = config_json['scrapers'][scraper_name]['package']
-    connector_name = config_json['scrapers'][scraper_name]['connector']
-    writer_name = config_json['scrapers'][scraper_name]['writer']
-    limit_count = config_json['scrapers'][scraper_name]['limit']
-    model_name = None
-    if 'model' in config_json['scrapers'][scraper_name]:
-        model_name = config_json['scrapers'][scraper_name]['model']
-
-    conn = importlib.import_module('connectors.' + connector_name).Connector(config_json['connectors'])
-    writer = importlib.import_module('writers.' + writer_name).Writer(config_json['writers'], name=target_id)
-    scraper = importlib.import_module('scrapers.' + package_name).Scraper(conn.api())
-    model = None
-    if model_name:
-        model = importlib.import_module('models.' + model_name).Model
-
-    for item in scraper.scrape(target_id, limit=limit_count):
-        if model:
-            from_func = getattr(model, "from_" + connector_name)
-            t = from_func(item)
-            writer.write(t)
-        else:
-            writer.write(item)
+    scrape(scraper_name, target_id)
